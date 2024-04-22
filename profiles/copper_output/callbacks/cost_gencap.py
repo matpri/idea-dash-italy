df8c98773c4eb9750959e682bdf40e79017d6da3
jkraasch <jkraasch@sfu.ca>
2024-04-22 15:46:10 -0700

    Refactor display attribute assignments in callback scripts
    Replaced `_s_style` assignments with `_pattern_style` and `_text_style` in several callback scripts to hide pattern and text displays within plot rendering. This refinement ensures display options can be set individually, enhancing modularity and flexibility for plot display configurations.

--- profiles/copper_output/callbacks/cost_gencap.py
+++ profiles/copper_output/callbacks/cost_gencap.py
@@ -177,6 +177,8 @@ def link(app):
             _r_style[idx] = {'display': 'block'}
             _y_style[idx] = {'display': 'none'}
             _s_style[idx] = {'display': 'block'}
+            _pattern_style[idx] = {'display': 'none'}
+            _text_style[idx] = {'display': 'none'}
             if _aggregates[idx] is not None:
                 _canvas[idx] = render_plot('Trend Over Years', data_handler.processed_data['COPPER Output']['Capacity Cost'],
                                            _aggregates[idx],
@@ -189,6 +191,8 @@ def link(app):
             _r_style[idx] = {'display': 'block'}
             _y_style[idx] = {'display': 'block'}
             _s_style[idx] = {'display': 'block'}
+            _pattern_style[idx] = {'display': 'none'}
+            _text_style[idx] = {'display': 'none'}
             if _aggregates[idx] is not None:
                 _canvas[idx] = render_plot('Pie Chart', data_handler.processed_data['COPPER Output']['Capacity Cost'],
                                            _aggregates[idx],
