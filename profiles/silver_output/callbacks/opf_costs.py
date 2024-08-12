13c34392abc35406a513a8caff511832543f69dd
jkraasch <jkraasch@sfu.ca>
2024-08-12 12:58:21 -0700

    Add timestep selection to OPF Costs plotting
    Introduced a new input for selecting timesteps in the OPF Costs plots. This allows users to view costs aggregated by hourly, daily, monthly, or yearly intervals. Updated the rendering logic to handle these new time groupings.

--- profiles/silver_output/callbacks/opf_costs.py
+++ profiles/silver_output/callbacks/opf_costs.py
@@ -21,6 +21,10 @@ def link(app):
             'index': ALL
         }, 'value'),
         Input({
+            'type': 'silver-opf_costs-time_step-select',
+            'index': ALL
+        }, 'value'),
+        Input({
             'type': 'silver-opf_costs-download-button',
             'index': ALL
         }, 'n_clicks'),
@@ -36,7 +40,7 @@ def link(app):
         }, 'data'),
         prevent_initial_call=True
     )
-    def update_opf_costs(_scenarios, _download, _canvas, _data):
+    def update_opf_costs(_scenarios,_tstep, _download, _canvas, _data):
         print('updating opf_costs plot')
         from main import data_handler
         ctx = dash.callback_context
@@ -60,7 +64,7 @@ def link(app):
                 break
 
         _canvas[idx] = render_plot(data_handler.processed_data['SILVER Output']['OPF Costs'],
-                                   _scenarios[idx])
+                                   _scenarios[idx], time_size=_tstep[idx])
 
 
         return _canvas, [dash.no_update for _ in _data]
