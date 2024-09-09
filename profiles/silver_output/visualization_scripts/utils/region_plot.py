7d4d2f3fbb11635eb11d8ac54faf90de6e168a23
jkraasch <jkraasch@sfu.ca>
2024-09-09 14:49:35 -0700

    Add dynamic plot types based on time entry counts
    Updated visualization scripts to dynamically choose plot types (pie, bar, stack) based on the count of unique time entries. Simplified code and improved readability by removing unnecessary comments and redundant operations.

--- profiles/silver_output/visualization_scripts/utils/region_plot.py
+++ profiles/silver_output/visualization_scripts/utils/region_plot.py
@@ -35,63 +35,77 @@ def render(df, scenario, title, x_axis_label, y_axis_label, time_size='hourly'):
     cols.remove('value')
     df_scen = df_scen.groupby(cols).sum(numeric_only=True).reset_index()
 
-    df_scen['time'] = pd.to_datetime(df_scen['time'])
-    # groupby time based on time_size
-    if time_size == 'daily':
-        df_scen['time'] = df_scen['time'].dt.strftime('%Y-%m-%d')
-    elif time_size == 'monthly':
-        df_scen['time'] = df_scen['time'].dt.strftime('%Y-%m')
-    elif time_size == 'yearly':
-        df_scen['time'] = df_scen['time'].dt.strftime('%Y')
-    else:
-        df_scen['time'] = df_scen['time'].dt.strftime('%Y-%m-%d %H:%M:%S')
-
-    cols = df_scen.columns.tolist()
-    # remove value column
-    cols.remove('value')
-    df_scen = df_scen.groupby(cols).sum(numeric_only=True).reset_index()
-
     can_supply = df_scen.sort_values(by=['time', 'variable'], key=lambda x: x.map(custom_sort_key))
     can_emissions = can_supply[can_supply['value'] != 0]
     techs = can_emissions.variable.unique().tolist()
 
-    # create stacked bar chart
-    for i, tech in enumerate(techs):
-        df_tech = can_emissions[can_emissions['variable'] == tech]
-        df_tech = df_tech.sort_values(by=['time'])
-        output = tech.split('|')[0]
-        tech_type = tech.split('|')[1]
-        unit = y_axis_label
-        fig.add_trace(go.Scatter(
-            x=df_tech['time'],
-            y=df_tech['value'],
-            name=tech,
-            mode='lines' if len(df_tech['time']) > 1 else 'markers',
-            # set line color to respective tech color
-            line=dict(color=utils.get_color(tech)),
-            marker=dict(color=utils.get_color(tech)),
-            stackgroup='one',
-            hovertemplate=f'<b>{tech_type}</b><br><br>' +
-                            f'Scenario: {scenario} <br>' +
-                              'Time: %{x}<br>' +
-                              f'{output}:'+ '%{y:.2f}' + ' '+f'{unit}<br>' +
-                              '<extra></extra>'
-        ))
-    fig.update_yaxes(showgrid=True)
-    fig.update_xaxes(
-        rangeslider_visible=True,
-        rangeselector=dict(
-            buttons=list([
-                dict(count=1, label="1d", step="day", stepmode="backward"),
-                dict(count=1, label="1m", step="month", stepmode="backward"),
-                dict(count=6, label="6m", step="month", stepmode="backward"),
-                dict(count=1, label="YTD", step="year", stepmode="todate"),
-                dict(count=1, label="1y", step="year", stepmode="backward"),
-                dict(step="all")
-            ])
-        )
-    )
+    # Determine the number of unique time entries
+    unique_times = can_emissions['time'].nunique()
 
+    if unique_times == 1:
+        # Create a pie chart
+        fig = go.Figure(data=[go.Pie(
+            labels=can_emissions['variable'],
+            values=can_emissions['value'],
+            hole=.3,
+            marker=dict(colors=[utils.get_color(tech) for tech in can_emissions['variable']])
+        )])
+        fig.update_traces(textposition='inside', textinfo='percent+label')
+        fig.update_layout(
+            title_text=title,
+            annotations=[dict(text=f'Total: {can_emissions["value"].sum():.2f} {y_axis_label}', showarrow=False)]
+        )
+    elif unique_times < 10:
+        # Create a bar plot
+        for tech in techs:
+            df_tech = can_emissions[can_emissions['variable'] == tech]
+            df_tech = df_tech.sort_values(by=['time'])
+            fig.add_trace(go.Bar(
+                x=df_tech['time'],
+                y=df_tech['value'],
+                name=tech,
+                marker_color=utils.get_color(tech),
+                hovertemplate=f'<b>{tech}</b><br><br>' +
+                              f'Scenario: {scenario} <br>' +
+                              'Time: %{x}<br>' +
+                              f'Value: %{{y:.2f}} {y_axis_label}<br>' +
+                              '<extra></extra>'
+            ))
+        fig.update_layout(barmode='stack')
+    else:
+        # Create a stacked area chart (original behavior)
+        for tech in techs:
+            df_tech = can_emissions[can_emissions['variable'] == tech]
+            df_tech = df_tech.sort_values(by=['time'])
+            output = tech.split('|')[0]
+            tech_type = tech.split('|')[1]
+            fig.add_trace(go.Scatter(
+                x=df_tech['time'],
+                y=df_tech['value'],
+                name=tech,
+                mode='lines',
+                line=dict(color=utils.get_color(tech)),
+                stackgroup='one',
+                hovertemplate=f'<b>{tech_type}</b><br><br>' +
+                              f'Scenario: {scenario} <br>' +
+                              'Time: %{x}<br>' +
+                              f'{output}: %{{y:.2f}} {y_axis_label}<br>' +
+                              '<extra></extra>'
+            ))
+        fig.update_yaxes(showgrid=True)
+        fig.update_xaxes(
+            rangeslider_visible=True,
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=1, label="1d", step="day", stepmode="backward"),
+                    dict(count=1, label="1m", step="month", stepmode="backward"),
+                    dict(count=6, label="6m", step="month", stepmode="backward"),
+                    dict(count=1, label="YTD", step="year", stepmode="todate"),
+                    dict(count=1, label="1y", step="year", stepmode="backward"),
+                    dict(step="all")
+                ])
+            )
+        )
 
     fig.layout.autosize = True
     return fig
