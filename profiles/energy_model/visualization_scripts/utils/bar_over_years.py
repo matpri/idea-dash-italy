530b1b43773e0711f5b732a712174b1b053e3287
jkraasch <jkraasch@sfu.ca>
2024-11-06 07:54:45 -0800

    Adding a total annotation in the bar plots for the case where both positive and negative values are present

--- profiles/energy_model/visualization_scripts/utils/bar_over_years.py
+++ profiles/energy_model/visualization_scripts/utils/bar_over_years.py
@@ -37,6 +37,14 @@ def plot(df, scenarios, region, aggregate, title, x_axis_label, y_axis_label, to
                         marker_pattern_shape=scen_patterns if pattern_active else None,
                         textposition='auto' if text_active else None, text=tech if text_active else None,
                         hovertemplate=f'<b>{tech}</b><br><br>' + 'Year: %{x[0]}<br>' + f'Region: {region}<br>' + 'Scenario: %{x[1]}<br>'+f'{tooltip_name}'+': %{y:.2f} '+f'{unit}'+'<br>Total: %{customdata:.2f} '+f'{unit}'+'<br><extra></extra>')
+        
+        total_data = df_scen.groupby(['time', 'scenario'])['value'].sum().reset_index()
+        x = []
+        x.append(total_data['time'].values)
+        x.append(total_data['scenario'].values)
+        fig.add_trace(go.Scatter(x=x, y=total_data["value"], name='Total', mode='markers', 
+                                 marker=dict(size=3, color='rgba(0, 0, 255, 0.6)'),  # Smaller size and softer blue color
+                                 hovertemplate='Total: %{y:.2f} '+f'{unit}'+'<br>Year: %{x[0]}<br>' + f'Region: {region}<br>' + 'Scenario: %{x[1]}<br><extra></extra>'))
         fig.update_layout(barmode='relative')
         fig.update_yaxes(showgrid=True)
         if df_scen.empty:
