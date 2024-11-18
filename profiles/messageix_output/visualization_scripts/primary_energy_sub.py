122d25d170cfc8eaa823e4a226b3af296a3e266b
jkraasch <jkraasch@sfu.ca>
2024-11-18 14:13:46 -0800

    Improving map titles

--- profiles/messageix_output/visualization_scripts/primary_energy_sub.py
+++ profiles/messageix_output/visualization_scripts/primary_energy_sub.py
@@ -21,7 +21,8 @@ def render_plot(type, df, aggregate, scenarios, region, year, scenario, pattern_
         plot_info = plot_settings['Primary Energy (substitution method)']['Pie Chart']
         return pie_chart.plot(df, scenario, region, year, aggregate, plot_info['title'], plot_info['x_label'], plot_info['y_label'])
     elif type == 'Map Plot':
-        return map_plot.plot_map(df, scenario, year, variable, aggregate)
+        title = plot_settings['Primary Energy (substitution method)']['Map']['title']
+        return map_plot.plot_map(df, scenario, year, title, name, unit)
     else:
         plot_info = plot_settings['Primary Energy (substitution method)']['By Region']
         return bar_over_regions.plot(df, scenarios, aggregate, year, plot_info['title'], plot_info['x_label'], plot_info['y_label'], name, unit, pattern_active=pattern_active, text_active=text_active)
