d6639024fc0b3a010ae5315fe133f996309ca745
jkraasch <jkraasch@sfu.ca>
2024-11-29 14:19:25 -0800

    using mean for percentage in overview

--- profiles/macromodel/macromodel.py
+++ profiles/macromodel/macromodel.py
@@ -229,6 +229,15 @@ class Macromodel(BaseProfile):
             # only keep CAN
             full_df = full_df[full_df['region'] == 'CAN']
 
+            # aggregate with groupby
+            percentages = full_df[full_df['unit'].str.contains('percent') | full_df['unit'].str.contains('%')]
+            full_df = full_df[~full_df['unit'].str.contains('percent') & ~full_df['unit'].str.contains('%')]
+
+            full_df = full_df.groupby(['scenario', 'variable', 'time', 'region', 'unit']).sum(numeric_only=True).reset_index()
+            percentages = percentages.groupby(['scenario', 'variable', 'time', 'region', 'unit']).mean(numeric_only=True).reset_index()
+            full_df = pd.concat([full_df, percentages], ignore_index=True)
+
+
             # Append overview data to processed data
             overview_df = full_df[full_df['scenario'].isin(overview_scenarios)]
             processed_data.append(
