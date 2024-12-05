6fa97637810948d81f806ddc3d0a26a84704a904
jkraasch <jkraasch@sfu.ca>
2024-12-05 15:03:14 -0800

    Fixing data indexing for new overview plot setup

--- profiles/cims_output/callbacks/requested_quantities.py
+++ profiles/cims_output/callbacks/requested_quantities.py
@@ -174,13 +174,15 @@ def link(app):
         ctx = dash.callback_context
         trigger_id = eval(ctx.triggered[0]['prop_id'].split('.')[0])
 
+        _data = data_handler.processed_data['CIMS']['Overview']
+        _data = _data[_data['tab'] == 'Energy Demand']
+
         if 'cims-requested_quantities-download-button' in trigger_id['type']:
-            _data = dcc.send_data_frame(data_handler.processed_data['CIMS']['Energy Demand'].to_csv, "requested_quantities.csv")
+            _data = dcc.send_data_frame(_data.to_csv, "requested_quantities.csv")
             return _canvas, _r_style, _y_style, _data, _s_style, _m_style, _pattern_style, _text_style
 
         services = dash.no_update
         if 'cims-requested_quantities-sector-select' in trigger_id['type']:
-            _data = data_handler.processed_data['CIMS']['Energy Demand']
             _data = _data[_data['sector'] == _sector]
             services = _data[_data['technology'].isna()]['short_path'].unique().tolist()
 
@@ -213,7 +215,7 @@ def link(app):
             _pattern_style = {'display': 'block'}
             _p_style = {'display': 'block'}
             _text_style = {'display': 'block'}
-            _canvas = render_plot(_representation,'By Year', data_handler.processed_data['CIMS']['Energy Demand'],
+            _canvas = render_plot(_representation,'By Year', _data,
                                        _scenarios,
                                        _regions,
                                        _years, scenario=_scenario,
@@ -227,7 +229,7 @@ def link(app):
             _p_style = {'display': 'block'}
             _pattern_style = {'display': 'none'}
             _text_style = {'display': 'none'}
-            _canvas = render_plot(_representation,'Trend Over Years', data_handler.processed_data['CIMS']['Energy Demand'],
+            _canvas = render_plot(_representation,'Trend Over Years', _data,
                                        _scenarios,
                                        _regions,
                                        _years, scenario=_scenario, sector=_sector, service=_service, fuel=_fuel)
@@ -239,7 +241,7 @@ def link(app):
             _y_style = {'display': 'block'}
             _pattern_style = {'display': 'none'}
             _text_style = {'display': 'none'}
-            _canvas = render_plot(_representation,'Pie Chart', data_handler.processed_data['CIMS']['Energy Demand'],
+            _canvas = render_plot(_representation,'Pie Chart', _data,
                                        _scenarios,
                                        _regions,
                                        _years, scenario=_scenario, sector=_sector, service=_service, fuel=_fuel)
@@ -252,7 +254,7 @@ def link(app):
             _p_style = {'display': 'block'}
             _pattern_style = {'display': 'block'}
             _text_style = {'display': 'block'}
-            _canvas = render_plot(_representation,'By Region', data_handler.processed_data['CIMS']['Energy Demand'],
+            _canvas = render_plot(_representation,'By Region', _data,
                                        _scenarios,
                                        _regions,
                                        _years, scenario=_scenario,
@@ -265,7 +267,7 @@ def link(app):
             _p_style = {'display': 'none'}
             _pattern_style = {'display': 'none'}
             _text_style = {'display': 'none'}
-            _canvas = render_plot(_representation,'Sankey', data_handler.processed_data['CIMS']['Energy Demand'],
+            _canvas = render_plot(_representation,'Sankey', _data,
                                        _scenarios,
                                        _regions,
                                        _years, scenario=_scenario, sector=_sector, service=_service, fuel=_fuel)
