54125470afb4a9138fcaf5ca7f8b83e6b77bf6d5
jkraasch <jkraasch@sfu.ca>
2025-04-22 11:16:04 -0700

    Improve profile and visualization retrieval in plot handling

--- callbacks/plot_handling.py
+++ callbacks/plot_handling.py
@@ -56,8 +56,12 @@ def update_plot(_figs, _figs2, _fig3, _fig4, _md1, _plots):
 
     triggered_value = ctx.triggered[0]['value']
     if triggered_id['type'] == ids.FIGURE:
-        profile = triggered_id['profile']
-        viz = triggered_id['viz']
+        profile = triggered_id.get('profile', None)
+        if profile is None:
+            profile = triggered_id['model']
+        viz = triggered_id.get('viz', None)
+        if viz is None:
+            viz = triggered_id['name']
         desc = None
         for _, report in data_handler.reports.items():
             if profile in report.descriptions:
