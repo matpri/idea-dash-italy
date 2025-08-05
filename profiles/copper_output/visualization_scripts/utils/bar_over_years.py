1811e1c15b196d5e5427667ca23e45dc8a5e29eb
jkraasch <jkraasch@sfu.ca>
2025-08-05 13:45:46 -0700

    Remove unused legend_traceorder update in bar_over_years.py for cleaner layout

--- profiles/copper_output/visualization_scripts/utils/bar_over_years.py
+++ profiles/copper_output/visualization_scripts/utils/bar_over_years.py
@@ -49,7 +49,7 @@ def plot(df, scenarios, region, aggregate, title, x_axis_label, y_axis_label, to
                             legendrank=1,
                             textposition='auto' if text_active else None, text=tech if text_active else None,
                             hovertemplate=f'<b>{tech}</b><br><br>' + 'Year: %{x[0]}<br>' + f'Region: {region}<br>' + 'Scenario: %{x[1]}<br>'+f'{tooltip_name}'+': %{y:.2f} '+f'{unit}'+'<br>Total: %{customdata:.2f} '+f'{unit}'+'<br><extra></extra>')
-        fig.update_layout(barmode='relative', legend_traceorder="reversed")
+        fig.update_layout(barmode='relative') #, legend_traceorder="reversed"
         fig.update_layout(legend=dict(groupclick="toggleitem"))
         fig.update_yaxes(showgrid=True)
         if df_scen.empty:
