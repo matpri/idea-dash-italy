d5db8d028d3758a3f10392bb66d2eaf9b9a97a6c
jkraasch <jkraasch@sfu.ca>
2025-10-23 10:38:13 -0700

    Update value conversion from billions to millions in cost and emissions calculations

--- profiles/natem_output/processing_scripts/cost_fom.py
+++ profiles/natem_output/processing_scripts/cost_fom.py
@@ -103,6 +103,6 @@ def process(data):
         dfs.append(df)
     full_df = pd.concat(dfs)
     full_df['unit'] = '$ Billions'
-    # full_df['value'] = full_df['value'].div(1e9)
+    full_df['value'] = full_df['value'].mul(1e6)
     
     return full_df
--- profiles/natem_output/processing_scripts/cost_vom.py
+++ profiles/natem_output/processing_scripts/cost_vom.py
@@ -110,6 +110,7 @@ def process(data):
         dfs.append(df)
     full_df = pd.concat(dfs)
     full_df['unit'] = '$ Billions'
-    full_df['value'] = full_df['value'].div(1e9)
+    full_df['value'] = full_df['value'].mul(1e6)
+    full_df['value'] = full_df['value']
 
     return full_df
