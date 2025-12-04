2334611fa833824034212c13404af8b7031f9518
jkraasch <jkraasch@sfu.ca>
2025-12-04 10:39:11 -0800

    Update merge method to use outer join for reference scenario comparisons

--- profiles/energy_model/callbacks/generation_capacity.py
+++ profiles/energy_model/callbacks/generation_capacity.py
@@ -270,7 +270,7 @@ def link(app):
                 print("No Reference scenario found for comparison.")
             else:
                 merged = df.merge(reference_data, on=['model', 'version', 'time', 'variable', 'region'],
-                                  suffixes=('', '_ref'))
+                                  suffixes=('', '_ref'), how='outer')
                 merged['value_ref'] = merged['value_ref'].fillna(0)
                 merged['value'] = merged['value'] - merged['value_ref']
                 df = merged[['scenario', 'time', 'variable', 'region', 'value']]
